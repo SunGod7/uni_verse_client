297d0e48dc19ed37ced61fd96653e9f7f291c233
SunGod7 <sun7ruler@gmail.com>
2022-08-29 16:32:36 -0400

    new react app 4 time

--- src/components/lyrics/ShowLyric.js
+++ src/components/lyrics/ShowLyric.js
@@ -4,7 +4,7 @@ import { useParams, useNavigate } from 'react-router-dom'//,
 // useParams will allow us to see our parameters
 // useNavigate will allow us to navigate to a specific page
 
-import { Container, Card, Button, Image } from 'react-bootstrap'
+import { Container, Card, Button } from 'react-bootstrap'
 
 import LoadingScreen from '../shared/LoadingScreen'
 import { getOneLyric, updateLyric, removeLyric } from '../../api/lyrics'
